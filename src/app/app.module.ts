5e435094db2d7f7dda948704eba106fd9cc7cf3d
Tania2106 <tania.trujillole@campusucc.edu.co>
2023-05-13 17:26:49 -0500

    Cambio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,7 +24,6 @@ import { ComicsComponent } from './comics/comics.component';
     NavbarComponent,
     ComicComponent,
     ComicsComponent
-
   ],
   imports: [
     BrowserModule,
--- src/app/comics/comic/shared/comics-api.service.ts
+++ src/app/comics/comic/shared/comics-api.service.ts
@@ -10,7 +10,8 @@ import { map } from 'rxjs/operators';
 export class ComicsApiService {
   PUBLIC_KEY = '';
   HASH = ''; 
-  URL_API = `https://gateway.marvel.com/v1/public/series?ts=1&apikey=cbccd95e75c5417da8a7b920a58a2a6e&hash=e9aa3aa6dab54a746bb7b271c32efc83`;
+  URL_API = `https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=cbccd95e75c5417da8a7b920a58a2a6e&hash=e9aa3aa6dab54a746bb7b271c32efc83
+  `;
  
   constructor( private http: HttpClient) { }
 
